cccff9a9707c07814bfeaef2c03fbd3921984ad8
ffalt <ffalty@gmail.com>
2016-12-13 15:42:57 +0100

    aggregate field "offentlig_finansiär" and throw away detail info

--- webapps/projektbank.tillvaxtverket.se-projektbanken/scraper.js
+++ webapps/projektbank.tillvaxtverket.se-projektbanken/scraper.js
@@ -14,10 +14,29 @@ var result = json.rows.map(function (row, i) {
 	var o = {_source: 'http://projektbank.tillvaxtverket.se/projektbanken#page=eruf'};
 	row.forEach(function (cell, j) {
 		var cat = cols[j].name.toLowerCase().replace(':', '').replace(/\//g, '_').replace(/ /g, '_');
+
+		if (cat == 'offentlig_finansiär') {
+			var value = 0;
+			cell.split('||').filter(function (item) {
+				return item.trim().length > 0;
+			}).forEach(function (item) {
+				item = item.split('=');
+				var s = item[1].replace(/ /g, '');
+				if (isNaN(s))
+					console.log('WARNING', s);
+				else {
+					value += parseInt(s, 10);
+				}
+			});
+			cell = value;
+		}
+
 		if (cell.v)
 			o[cat] = cell.o;
 		else
 			o[cat] = cell;
+
+
 	});
 	o._source = 'http://projektbank.tillvaxtverket.se/projektbanken';
 	return o;
